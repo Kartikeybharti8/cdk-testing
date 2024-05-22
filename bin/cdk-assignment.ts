1934ce692a0f58177ead9efb911d0ccb51040624
ittKartikey <kartikey.bharti@intimetec.com>
2024-05-22 23:57:17 +0530

    Code for assignment 2nd

--- bin/cdk-assignment.ts
+++ bin/cdk-assignment.ts
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
-import { devStackProps, prodStackProps } from '../configuration_parameters'
+import { devStackProps } from '../configuration_parameters'
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkAssignmentStack } from '../lib/cdk-assignment-stack';
-import { CdkTestStack } from '../lib/cdk-test-stack';
 
 
 const app = new cdk.App();
 
-// new CdkAssignmentStack(app, 'CdkAssignmentStack', devStackProps);
-new CdkTestStack(app, 'CdkTestStackDev', devStackProps);
+new CdkAssignmentStack(app, 'CdkAssignmentStack', devStackProps);
